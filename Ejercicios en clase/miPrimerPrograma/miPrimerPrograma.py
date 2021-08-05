57661d5efdfbd37d3f5afa0041267ca0be6c4343
Alejandro Navas <alejandrolopez9544@gmail.com>
2021-08-05 09:22:53 -0600

    Ejercicio 05082021
    Primer ejercicio visto en clase. Utilidad de la funcion print

---
+++ Ejercicios en clase/miPrimerPrograma/miPrimerPrograma.py
@@ -0,0 +1,18 @@
+#Fundacion Kinal
+#Centro educativo tecnico laboral Kinal
+#Perito en electronica
+#cuarto grado.
+#Taller de electronica analogica
+#Alumno: aqui escribo mi nombre
+#Carnet: aqui escribo mi numero de carnet
+#Tarea: miPrimerPrograma
+
+#La tarea trata sobre como mostrar texto, en este un 
+#mensaje de bienvenida.
+
+
+print("Hola mi nombre [aqui escribo mi nombre]") #Imprime en pantalla mi nombre.
+
+
+
+
