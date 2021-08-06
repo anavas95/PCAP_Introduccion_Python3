002b4af1b481019ab5f4d93dc09a6e6e04fa5001
Alejandro Navas <alejandrolopez9544@gmail.com>
2021-08-05 21:51:33 -0600

    Tipos de datos
    Ejercicios de ayuda sobre los tipos de datos

---
+++ Ejercicios en clase/Variables/tipoDato_entero.py
@@ -0,0 +1,32 @@
+#Fundacion Kinal
+#Centro educativo tecnico laboral Kinal
+#Perito en electronica
+#cuarto grado.
+#Taller de electronica analogica
+#Alumno: Luis Alejandro Lopez Navas
+#Carnet: -------------
+#Actividad: Tipo de dato entero
+
+#Los tipos de datos enteros o tambien llamados int(abreviatura de interger), hacen 
+#referencia a un tipo de datos que almacenan numeros sin decimales. Y se 
+#declaran de la siguiente manera:
+
+_myVar = 2  #mi variable se llama _myVar que posee un valor numerico de 2.
+
+#Este tipo de variable se puede tanto imprimir como  operar con calculos
+#que deseamos.
+
+#por ejemplo: Deseo realizar la suma de 3 numeros
+
+_firstNumber = 5
+_secondNumber = 6
+_thirdNumber = 7
+
+#Realizo la suma de las 3 variables anteriormente declaras y las muestro 
+#en la consola de Windows.
+
+print('El valor de la primera variable es: ', _firstNumber)
+print('El valor de la segunda variable es: ', _secondNumber)
+print('El valor de la tercera variable es: ', _thirdNumber)
+
+print('La suma de las 3 variables anteriores es de: ', _firstNumber+_secondNumber+_thirdNumber)
---
+++ Ejercicios en clase/Variables/tipoDato_string.py
@@ -0,0 +1,24 @@
+#Fundacion Kinal
+#Centro educativo tecnico laboral Kinal
+#Perito en electronica
+#cuarto grado.
+#Taller de electronica analogica
+#Alumno: Luis Alejandro Lopez Navas
+#Carnet: -------------
+#Actividad: Tipo de dato String 
+
+#Los tipos de datos String hacen referencia a una frase o una palabra en especifico
+#y se declaran de la siguiente manera. 
+
+_myString = 'Esto es un string'
+
+#Python es capaz de reconocer al texto contenido dentro de comillas dobles
+#o simples como un un string.
+
+_myString_2 = "Esto tambien es un string"
+
+#Y se visualizan de la siguienta manera:
+
+print('Imprimo mi cadena de caracteres de prueba', _myString)
+print('Imprimo mi segunda cadena de caracteres de prueba', _myString_2)
+
