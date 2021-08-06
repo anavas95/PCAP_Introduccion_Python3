da1a57ed28620a89d2381615404acad4d37453ab
Alejandro Navas <alejandrolopez9544@gmail.com>
2021-08-05 22:12:02 -0600

    Tipos de datos
    Se agrega el tipo de dato booleano, como declararlo y una reseña breve de donde se utiliza.

---
+++ Ejercicios en clase/Variables/tipoDato_boolean.py
@@ -0,0 +1,20 @@
+#Fundacion Kinal
+#Centro educativo tecnico laboral Kinal
+#Perito en electronica
+#cuarto grado.
+#Taller de electronica analogica
+#Alumno: Luis Alejandro Lopez Navas
+#Carnet: -------------
+#Actividad: Tipo de dato bool.
+
+#Los datos tipo bool son utilizados para represtar unicamente dos estados
+#posibles: falso y verdadero. Por la connotacion del lenguaje de programacion
+#nos referiremos a estos como true(verdader) y como false(falso).
+
+#Para declarlos y utilizarlos usamos la siguiente forma:
+
+_booleanVar = True
+_booleanVar2 = False
+
+#Aunque los podamos imprimir y mostrar en pantalla, en este tipo de acciones no 
+#son cruciales, caso contrario cuando los utilizamos en control de flujo.
